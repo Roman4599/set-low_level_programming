ea3443a199dcb54f80952c7f3e7a6e3d6315dded
Roman4599 <mengestaroman@gmail.com>
2026-05-18 14:09:33 +0300

    Fix 10-print_comb2.c: remove unused variable j

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print all two-digit number combinations
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 0; i <= 99; i++)
+    {
+        putchar((i / 10) + '0');
+        putchar((i % 10) + '0');
+        if (i != 99)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    putchar('\n');
+
+    return (0);
+}
