966ecf3a31eae10abd1193a8ae09d282438fcec0
Roman4599 <mengestaroman@gmail.com>
2026-06-22 02:28:19 -0700

    Add 3-function_like_macro.h

---
+++ 0x0B-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * ABS - computes the absolute value of a number x
+ * @x: the number to compute the absolute value of
+ */
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
