f560e984b118581b33469dd7889c6780d6c10898
Roman4599 <mengestaroman@gmail.com>
2026-06-22 02:31:26 -0700

    Add 4-sum.h

---
+++ 0x0B-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * SUM - computes the sum of two numbers x and y
+ * @x: the first number
+ * @y: the second number
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif /* SUM_H */
